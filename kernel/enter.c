2909777151279d40355fdd3a36771eee6261fa02
Vikas Tikoo <vitikoo@microsoft.com>
2022-02-04 21:01:28 +0000

    Add ramfs based file system for storing POSIX shm objects: shmfs
    - For stat family calls, shmfs files return 9 for st_dev field
    
    Signed-off-by: Vikas Tikoo <vitikoo@microsoft.com>

--- kernel/enter.c
+++ kernel/enter.c
@@ -38,6 +38,7 @@
 #include <myst/procfs.h>
 #include <myst/pubkey.h>
 #include <myst/ramfs.h>
+#include <myst/shmfs.h>
 #include <myst/signal.h>
 #include <myst/stack.h>
 #include <myst/strings.h>
@@ -248,7 +249,7 @@ static int _init_tmpfs(const char* target, myst_fs_t** fs_out)
         ERAISE(-EINVAL);
     }
 
-    if (myst_init_ramfs(myst_mount_resolve, &fs) != 0)
+    if (myst_init_ramfs(myst_mount_resolve, &fs, 0) != 0)
     {
         myst_eprintf("cannot initialize file system: %s\n", target);
         ERAISE(-EINVAL);
@@ -298,7 +299,7 @@ static int _setup_ramfs(void)
 {
     int ret = 0;
 
-    if (myst_init_ramfs(myst_mount_resolve, &_fs) != 0)
+    if (myst_init_ramfs(myst_mount_resolve, &_fs, 0) != 0)
     {
         myst_eprintf("failed initialize the RAM file system\n");
         ERAISE(-EINVAL);
@@ -815,6 +816,9 @@ int myst_enter_kernel(myst_kernel_args_t* args)
     /* Setup devfs */
     devfs_setup();
 
+    /* Setup POSIX Shared Memory fs */
+    shmfs_setup();
+
     /* Create top-level proc entries */
     create_proc_root_entries();
 
@@ -1009,6 +1013,9 @@ int myst_enter_kernel(myst_kernel_args_t* args)
 
     /* Tear down the dev file system */
     devfs_teardown();
+
+    /* Tear down the posix shm file system */
+    shmfs_teardown();
 
     /* Tear down the RAM file system */
     _teardown_ramfs();
