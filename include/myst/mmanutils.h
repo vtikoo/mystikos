336aa7f2995e0927d4bd32a09598dc259c6ef4d3
Vikas Tikoo <vitikoo@microsoft.com>
2022-03-10 20:58:00 +0000

    Change file handle cleanup algorithm
    Earlier we were scanning the fdmappings vector to free the handles.
    Now the kernel maintains a list of active file handles and frees it on exit
    
    Signed-off-by: Vikas Tikoo <vitikoo@microsoft.com>

--- include/myst/mmanutils.h
+++ include/myst/mmanutils.h
@@ -6,6 +6,7 @@
 
 #include <myst/buf.h>
 #include <myst/fs.h>
+#include <myst/list.h>
 #include <myst/mman.h>
 #include <myst/refstr.h>
 #include <sys/types.h>
@@ -14,6 +15,7 @@
 
 typedef struct mman_file_handle
 {
+    myst_list_node_t base;
     myst_fs_t* fs;
     myst_file_t* file;
     ino_t inode;
